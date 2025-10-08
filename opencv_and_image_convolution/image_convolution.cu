b9229bc69f734f2acf1fb75c5c5946becd2d7108
mudtjn <mudit1978mars@gmail.com>
2025-10-08 21:01:21 +0000

    changing seperate memory management to unified memory

--- opencv_and_image_convolution/image_convolution.cu
+++ opencv_and_image_convolution/image_convolution.cu
@@ -1,13 +1,21 @@
 #include <opencv2/opencv.hpp>
 #define THREADS_PER_BLOCK 1024
+#define KERNEL_SIZE 31
 
 __global__
-void lighten_image(float *d_image, float *d_result_image, int N) {
+void lightenImage(float *d_image, float *d_result_image, int N) {
     // indx being pointed to
     int idx = (blockDim.x * blockIdx.x + threadIdx.x) ; 
     if(idx < N) {
         d_result_image[idx] = d_image[idx] * 3.0;
     }
+}
+
+void displayImage( cv::Mat &result_image) {
+    cv::Mat display; 
+    result_image.convertTo(display, CV_8UC3, 255.0); 
+    cv::imshow("Output", display);
+    cv::waitKey(0);
 }
 
 int main() {
@@ -24,31 +32,23 @@ int main() {
 
     std::cout << "Image size: " << imgFloat.rows << " x " << imgFloat.cols << std::endl;
 
-    float *d_image, *d_result_image, *h_result_image, *h_image;
+    float *initial_image, *result_image;
     size_t num_elements = imgFloat.total() * imgFloat.channels(); 
     size_t image_size = num_elements * sizeof(float); 
     
-    cudaMalloc(&d_image, image_size); 
-    cudaMalloc(&d_result_image, image_size); 
-    h_result_image = (float*) malloc(image_size); 
-    h_image = (float*) malloc(image_size); 
-    memcpy(h_image, imgFloat.ptr(), image_size);
-    cudaMemcpy(d_image, imgFloat.ptr(), image_size, cudaMemcpyHostToDevice); 
+
+
+    cudaMallocManaged(&initial_image, image_size); 
+    cudaMallocManaged(&result_image, image_size); 
+    cudaMemcpy(initial_image, imgFloat.ptr(), image_size, cudaMemcpyHostToDevice); 
 
     auto BLOCKS_PER_GRID = (num_elements + THREADS_PER_BLOCK - 1) /   THREADS_PER_BLOCK; 
 
-    lighten_image<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>> (d_image, d_result_image, image_size);
+    lightenImage<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>> (initial_image, result_image, image_size);
     cudaDeviceSynchronize(); 
     
-    cudaMemcpy(h_result_image, d_result_image, image_size, cudaMemcpyDeviceToHost); 
-
-    // cv::Mat result_image(imgFloat.rows, imgFloat.cols, CV_32FC3, h_image); 
-    cv::Mat result_image(imgFloat.rows, imgFloat.cols, CV_32FC3, h_result_image); 
-
-    cv::Mat display; 
-    result_image.convertTo(display, CV_8UC3, 255.0); 
-    cv::imshow("Output", display);
-    cv::waitKey(0);
+    cv::Mat final_image(imgFloat.rows, imgFloat.cols, CV_32FC3, result_image); 
+    displayImage(final_image); 
 
     return 0; 
 }
